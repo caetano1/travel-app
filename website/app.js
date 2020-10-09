009a1807ea6a097089511b7c93a9d9dc003ebe73
caetano <joaoluisfcaetano@gmail.com>
2020-10-09 16:43:51 -0300

    Adding the OpenWeatherAPI key

--- website/app.js
+++ website/app.js
@@ -3,3 +3,6 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+const apiKey = '781d27be53ccaae278c356ef46876e8a';
+
