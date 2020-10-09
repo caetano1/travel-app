5fd6443823b1e2aeecbb5753e4ae874dc8df420c
caetano <joaoluisfcaetano@gmail.com>
2020-10-09 16:43:11 -0300

    Adding the package.json and package-lock.json files

--- server.js
+++ server.js
@@ -15,14 +15,14 @@ app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 const cors = require('cors');
-app.use(cors);
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
-const port = 8000;
+const port = 3000;
 
 const server = app.listen(port, listening);
 
