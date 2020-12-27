34ecea094a036b27d8fe7651d51c6a8fa4493ea8
caetano <joaoluisfcaetano@gmail.com>
2020-12-27 19:26:23 -0300

    Add scripts to make the carrousel work
    This commit does:
    - Add the carrousel.js file to deal with the carrousel component
    - Adapt the updateui.js file that generates the components for the
    carrousel
    - Adapt the other files to this new component

---
+++ src/client/js/carrousel.js
@@ -0,0 +1,27 @@
+
+// Declares the functions that will allow the carroussel
+// to work properly
+
+const changeImage = (pace = 0) => {
+    let index
+    const imgs = document.getElementsByClassName('image')
+    // runs through the image objects to find the index of the current selected image
+    for (let i = 0; i < imgs.length; i++) {
+        if (imgs[i].classList.contains('selected-image')) {
+            index = i
+            // removes class name from previous selected image
+            imgs[i].classList.remove('selected-image');
+        }
+    }
+    showImage(index += pace, imgs)
+}
+
+const showImage = (i, images) => {
+    let index
+    if (i < 0) index = images.length - 1 ;
+    else if (i >= images.length) index = 0;
+    else index = i;
+    images[index].classList.add('selected-image');
+}
+
+export { changeImage, showImage }
