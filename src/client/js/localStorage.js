bcbf1d73af2b03c52c13a9c110063c86dcd21b97
caetano <joaoluisfcaetano@gmail.com>
2020-12-27 21:09:51 -0300

    Allow user to erase search
    This commit does:
    - Add the button to index.html
    - Implement the code to save the search info in the local storage
    - Implement the code to fetch the info saved in the local storage when
    the user closes and opens the browser
    - Implement the code to clear local storage (and therefore the search
    made previously)

---
+++ src/client/js/localStorage.js
@@ -0,0 +1,19 @@
+// Functions needed to store user's input after closing browser
+// and to erase current data
+
+import { updateUI } from './updateUi'
+
+const eraseLocalStorage = () => {
+    localStorage.clear();
+    location.reload();
+};
+
+const checkStoredInfo = () => {
+    console.log(localStorage)
+    if (localStorage.sessionData) {
+        const storedData = JSON.parse(localStorage.getItem('sessionData'));
+        updateUI(storedData);
+    }
+};
+
+export { eraseLocalStorage, checkStoredInfo }
